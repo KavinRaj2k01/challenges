85904b8e0d39f478b5123792935c65d89cf55dfd
KavinRaj2k01 <kavinrajlab@protonmail.com>
2021-05-14 16:26:25 +0530

    initial commit

---
+++ quiz-app/client-side/quiz-app-front/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div><MainScreen :userName="Kavin"> </MainScreen></div>
+</template>
+
+<script>
+import MainScreen from './screens/MainScreen'
+export default {
+  name: 'App',
+  components: {
+    MainScreen
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ quiz-app/client-side/quiz-app-front/src/screens/MainScreen.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+    <h1>Quiz-App</h1>
+    <p> {userName} </p>
+    <button> Start </button></div>
+</template>
+<script>
+export default
+{
+    name:"MainScreen",
+    props: ["userName"]
+}
+</script>
